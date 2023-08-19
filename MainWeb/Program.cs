40d8940e6e70330bebf2ba1b979682932eb34972
Anil Shrestha <ashre7@yahoo.com>
2023-08-19 18:24:35 +0545

    Upload Modules and singleton ModuleService

--- MainWeb/Program.cs
+++ MainWeb/Program.cs
@@ -9,7 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
-builder.Services.AddScoped<ModulesService>();
+builder.Services.AddSingleton<IModulesService, ModulesService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
--- MainWeb/Services/ModulesService.cs
+++ MainWeb/Services/ModulesService.cs
@@ -3,38 +3,69 @@ using Modular.Shared;
 
 namespace MainWeb.Services;
 
-public class ModulesService
+public interface IModulesService
 {
-    public List<Assembly> ModuleAssemblies { get; } = new();
-    public List<IModule> Modules { get; } = new();
+    List<Assembly> ModuleAssemblies { get; }
+    List<IModule> Modules { get; }
+
+    void SyncModules();
+}
+
+public class ModulesService : IModulesService
+{
+    public List<Assembly> ModuleAssemblies { get; private set; } = new();
+    public List<IModule> Modules { get; private set; } = new();
 
     public ModulesService()
     {
+        SyncModules();
+    }
+
+    public void SyncModules()
+    {
         // var context = new AssemblyLoader(@$"{Environment.CurrentDirectory}\Modules\Modular.HrModule.dll");
-        
-        var files = Directory.GetFiles(@$"{Environment.CurrentDirectory}\Modules", "Modular.*Module.dll");
-        // var files = Directory.GetFiles(@$"{Environment.CurrentDirectory}\Modules", "Modular.*.dll");
-        foreach (var file in files)
+        ModuleAssemblies = new();
+        Modules = new();
+        var fileinfos = new DirectoryInfo(@$"{Environment.CurrentDirectory}\Modules").GetFiles("Modular.*Module.dll")
+                        .OrderByDescending(x => x.CreationTime).ThenBy(x => x.Name);
+        foreach (var fileinfo in fileinfos)
         {
-            //TODO: optimize assembly loading using cache and possibly singleton with reload function
+
+            // }
+            // var files = Directory.GetFiles(@$"{Environment.CurrentDirectory}\Modules", "Modular.*Module.dll");
+            // // var files = Directory.GetFiles(@$"{Environment.CurrentDirectory}\Modules", "Modular.*.dll");
+            // foreach (var file in files)
+            // {
             var context = new AssemblyLoader("");
-            // System.Console.WriteLine(file);
-            var assembly = context.LoadFromStream(new MemoryStream(File.ReadAllBytes(file)));
+            System.Console.WriteLine("----------------------------------");
+            System.Console.WriteLine(fileinfo.Name);
+            var assembly = context.LoadFromStream(new MemoryStream(File.ReadAllBytes(fileinfo.FullName)));
             if (assembly != null)
             {
-                var modules = assembly.GetTypes().Where(type=>typeof(IModule).IsAssignableFrom(type)).Select(x=>Activator.CreateInstance(x) as IModule);
-                if(modules == null || modules.Any() ==false) {
+                System.Console.Write(assembly.GetName().Name);
+                var modules = assembly.GetTypes().Where(type => typeof(IModule).IsAssignableFrom(type)).Select(x => Activator.CreateInstance(x) as IModule);
+                if (modules == null || modules.Any() == false)
+                {
                     context.Unload();
+                    System.Console.WriteLine("............UnLoaded !!");
+
+                    continue;
                 }
-                Modules.AddRange(modules!);
-                ModuleAssemblies.Add(assembly);
+
+                if (!ModuleAssemblies.Any(x => x.GetName().Name == assembly.GetName().Name))
+                {
+                    Modules.AddRange(modules!);
+                    ModuleAssemblies.Add(assembly);
+                    System.Console.WriteLine("............Loaded !!");
+                }
+                else
+                    System.Console.WriteLine("............Skipped !!");
             }
         }
         // var assembly = context.LoadFromAssemblyPath(@$"{Environment.CurrentDirectory}\Modules\Modular.HrModule.dll");
         // var assembly = .LoadFromAssemblyName(new AssemblyName($"Modular.HrModule"));
         // if (assembly != null)
-            // ModuleAssemblies.Add(assembly);
+        // ModuleAssemblies.Add(assembly);
     }
-
 
 }
